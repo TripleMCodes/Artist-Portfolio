d4ec936a7a8f6a919184d46214eea14c12c96b44
TripleMCodes <khona6047@gmail.com>
2026-03-13 11:44:36 +0200

    add last updated date

--- vickykae_portfolio/templates/terms/terms.js
+++ vickykae_portfolio/templates/terms/terms.js
@@ -4,3 +4,12 @@ const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
     })
+
+const now = new Date();
+const oneMonthAgo = new Date(
+now.getFullYear(),
+now.getMonth() - 1,
+now.getDate()
+).toISOString().split('T')[0];
+lastUpdatedDate = document.querySelector('.last-updated')
+lastUpdatedDate.innerText = oneMonthAgo;
